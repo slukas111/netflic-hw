634242bf16fbda4c3377c7ea7b03cbbb4973d294
Sasha Lukas <sashamati@gmail.com>
2020-10-07 18:10:14 -0700

    updated components for app, created a request API with key, started the axios and installed to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import './Row'
 
 function App() {
   return (
     <div className="Netflix Clone">
       <header className="App-header">
-        <h1> This is the main page </h1>
+        <h1> Netflix Clone </h1>
+        <br />
+        <p> This is homework React Project</p>
+        <hr />
+
+
       </header>
     </div>
   );
---
+++ src/requests.js
@@ -0,0 +1,21 @@
+const API_KEY = "45cf8b9e5306495bf848eb8ab9bd82fa"
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`
+}
+
+export default requests;
+
+// 45cf8b9e5306495bf848eb8ab9bd82fa API Key
+// Ex API request https://api.themoviedb.org/3/movie/550?api_key=45cf8b9e5306495bf848eb8ab9bd82fa
+
+/*
+[√]INSTALL AXIOS
+[√]API KEY
+[]CREATE SEPERATE FILE FOR REQUESTS
+[]COMPONENT FOR ROWS
+[]DISPLAY MOVIES
+[]SIDE LEFT & RIGHT SCROLL
+[]PRACTICE JSON FILE
+*/
